831ab89d2fa873e8d33f5ca4b7a4f93c2b877024
Thong Dam Nhat <thong.dam@procivis.ch>
2021-05-06 18:44:47 +0700

    finish get data a detail page

--- src/store/constants/index.js
+++ src/store/constants/index.js
@@ -7,3 +7,6 @@ export const RE_RENDERED = 'RE_RENDERED';
 export const GET_POSTS = 'GET_POSTS';
 export const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
 export const GET_POSTS_FAIL = 'GET_POSTS_FAIL';
+
+export const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
+export const GET_POST_FAIL = 'GET_POST_FAIL';
