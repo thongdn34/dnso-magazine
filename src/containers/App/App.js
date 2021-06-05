a2ccb96deb099977dd84e6bb2ed0bb385bfff76c
Jack_Jimson <tiendatmema@gmail.com>
2021-06-05 15:54:19 +0700

    test SEO for site

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import Routes from "../__Routes";
 import ScrollTopButton from "../../components/ScrollTopButton";
 import ScrollToTop from "../../hooks/ScrollToTop";
+import { Helmet } from "react-helmet";
 
 const App = (props) => {
   const { error, success } = props;
@@ -11,6 +12,34 @@ const App = (props) => {
   if (success) toast.success(success);
   return (
     <Fragment>
+      <Helmet>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <title>{props.title}</title>
+        <meta name="description" content={props.description}/>
+        <meta name="keywords"/>
+        <meta name="author"/>
+        <meta name="twitter:card" content="summary"/>
+        <meta name="twitter:site"/>
+        <meta name="twitter:image"/>
+        <meta name="twitter:description"/>
+        <meta property="og:url"/>
+        <meta property="og:title"/>
+        <meta property="og:description"/>
+        {/* <meta property="og:image"/> */}
+        <meta property="og:image" itemprop="thumbnailUrl" content="https://s1.vnecdn.net/vnexpress/restruct/i/v391/logo_default.jpg"></meta>
+        <meta property="og:type" content="website"/>
+        <meta name="robots" content="index, follow"/>
+        <meta name="theme-color" content="#ff9900"/>
+        <meta name="msapplication-TileColor" content="#ff9900"/>
+        <meta name="msapplication-config" content="undefinedbrowserconfig.xml"/>
+        <link rel="manifest" href="undefinedmanifest.json"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="undefinedassets/icons/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="undefinedassets/icons/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="undefinedassets/icons/favicon-16x16.png"/>
+        <link rel="shortcut icon" href="undefinedassets/icons/favicon.ico"/>
+      </Helmet>
       {props.loading && <h1>loading...</h1>}
       <ScrollToTop />
       <Routes />
@@ -22,8 +51,8 @@ const App = (props) => {
 
 const MapStateToProps = (state) => {
   return {
-    error: state.meta.error,
-    success: state.meta.success
+    title: state.meta.title,
+    description: state.meta.description
   };
 };
 
